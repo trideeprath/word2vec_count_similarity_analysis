44111627a5da0b0b18b1c9f1ef074cb2c9cf902a
Trideep Rath <trideeprath1991@gmail.com>
2017-07-20 23:32:30 -0400

    code changed for magnitude

--- similarity_analysis.py
+++ similarity_analysis.py
@@ -2,11 +2,12 @@ from gensim.models import Word2Vec
 import numpy as np
 import matplotlib.pyplot as plt
 
-fname = "/home/trideep/Downloads/embeddings/win5_500mil.txt"
+#fname = "/home/trideep/Downloads/embeddings/win5_500mil.txt"
+fname = "/home/trideep/Downloads/GoogleNews-vectors-negative300.bin"
 count_dict = {}
 count_distance = []
 
-model = Word2Vec.load_word2vec_format(fname, binary=False)
+model = Word2Vec.load_word2vec_format(fname, binary=True)
 
 for word in model.vocab:
     count_dict[word] = model.vocab[word].count
@@ -15,9 +16,12 @@ count_dict= sorted(count_dict.items(), key=lambda x: x[1])
 
 for word_count in count_dict:
     count = word_count[1]
-    distance = np.linalg.norm(model[word_count[0]] - np.zeros(300))
+    #distance = np.linalg.norm(model[word_count[0]] - np.zeros(300))
+    distance = np.linalg.norm(model[word_count[0]])
     count_distance.append((word_count[0], count, distance))
 
+print(count_distance[0:100])
+print(count_distance[len(count_distance)-100: len(count_distance)-1])
 # plotting the moving average of distance of words in increasing order of count
 N = 20
 cumsum, moving_aves = [0], []
@@ -28,6 +32,6 @@ for i, x in enumerate([x[2] for x in count_distance], 1):
         moving_aves.append(moving_ave)
 
 plt.plot(moving_aves)
-plt.ylabel('distance')
+plt.ylabel('magnitude')
 plt.xlabel('word index by sorted count')
 plt.show()
